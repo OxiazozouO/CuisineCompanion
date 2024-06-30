1402b62eb038fef9169675557c22cc7ed7ea60e5
OxiazozouO <1714050472@qq.com>
2024-06-30 23:51:40 +0800

    结档

--- Experiment/Helper/MessageBoxHelper.cs
+++
@@ -1,9 +0,0 @@
-namespace Tmp.Helper;
-
-public static class MessageBoxHelper
-{
-    public static void ShowError(this Exception e)
-    {
-        MessageBox.Show(e.ToString(), "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
-    }
-}
--- Experiment/Views/Form3.cs
+++
@@ -1,9 +0,0 @@
-namespace Experiment.Views;
-
-public partial class Form3 : Form
-{
-    public Form3()
-    {
-        InitializeComponent();
-    }
-}
